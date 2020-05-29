295a7e8462d2b954c37115f241c3bea330eafd2b
Isaac Adamu <isaacmagaji1@gmail.com>
2020-05-29 17:32:35 +0100

    formatting the output using json

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ let request = require("request");
 
 let apiKey = "472430044e980f10dbf2bcbd0a0b6eee";
 let city = "jos";
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+let zipCode = 930271;
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&zip={zipCode}&appid=${apiKey}`;
 
 request(url, function (err, response, body) {
   if (err) {
     console.log("error:", error);
   } else {
-    console.log("body:", body);
+    let weather = JSON.parse(body);
+    console.log("body:", weather);
   }
 });
